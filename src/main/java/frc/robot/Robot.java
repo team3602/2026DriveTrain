ea893f345d552acdd4e0b32009d59f2f53bce684
Team 3602 <167370887+clydestale158@users.noreply.github.com>
2026-01-15 19:11:20 -0500

    Smart Dashboard
    Tried to add the camera TX to dashboard unsuccessfully.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,9 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.LimelightHelpers;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -27,7 +30,9 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
-        CommandScheduler.getInstance().run(); 
+        CommandScheduler.getInstance().run();
+        double tx = LimelightHelpers.getTX("primary");
+        SmartDashboard.putNumber("my heading", tx);
     }
 
     @Override
